466becfc03cbeef2e09b2adbe5090f8dbabdf29f
Jatin Rampal <jatinrampal@hotmail.com>
2018-11-28 01:58:58 -0500

    Initial Commit

---
+++ tvOS-SpriteKit-Assignment/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  tvOS-SpriteKit-Assignment
+//
+//  Created by Jatin Rampal on 2018-11-28.
+//  Copyright © 2018 Jatin Rampal. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
